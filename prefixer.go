f312a9d1d2f167847e9a6b6cc60e8fbe9e64e985
Vojtech Vitek (V-Teq) <vojtech.vitek@pressly.com>
2015-03-10 14:03:53 -0400

    Implement io.WriterTo, so io.Copy() don't buffer until EOF is reached

--- prefixer.go
+++ prefixer.go
@@ -5,8 +5,9 @@ import (
 	"io"
 )
 
-// Prefixer implements io.Reader. It reads data from the
-// underlying reader and prepends every line with a given string.
+// Prefixer implements io.Reader and io.WriterTo. It reads
+// data from the underlying reader and prepends every line
+// with a given string.
 type Prefixer struct {
 	reader *bufio.Reader
 	prefix []byte
@@ -45,6 +46,7 @@ func (r *Prefixer) Read(p []byte) (n int, err error) {
 
 		// Read new line, including delim.
 		r.unread, err = r.reader.ReadBytes('\n')
+
 		if err == io.EOF {
 			r.eof = true
 		}
@@ -53,15 +55,65 @@ func (r *Prefixer) Read(p []byte) (n int, err error) {
 		if len(r.unread) == 0 {
 			return n, err
 		}
+
 		// Some new data, prepend prefix.
 		// TODO: We could write the prefix to r.unread buffer just once
 		//       and re-use it instead of prepending every time.
 		r.unread = append(r.prefix, r.unread...)
 
 		if err != nil {
-			// The underlying Reader already returned EOF, but we still
-			// have some unread data to send, thus clear the error.
 			if err == io.EOF && len(r.unread) > 0 {
+				// The underlying Reader already returned EOF, but we still
+				// have some unread data to send, thus clear the error.
+				return n, nil
+			}
+			return n, err
+		}
+	}
+	panic("unreachable")
+}
+
+func (r *Prefixer) WriteTo(w io.Writer) (n int64, err error) {
+	for {
+		// Write unread data from previous read.
+		if len(r.unread) > 0 {
+			m, err := w.Write(r.unread)
+			n += int64(m)
+			if err != nil {
+				return n, err
+			}
+			r.unread = r.unread[m:]
+			if len(r.unread) > 0 {
+				return n, nil
+			}
+		}
+
+		// The underlying Reader already returned EOF, do not read again.
+		if r.eof {
+			return n, io.EOF
+		}
+
+		// Read new line, including delim.
+		r.unread, err = r.reader.ReadBytes('\n')
+
+		if err == io.EOF {
+			r.eof = true
+		}
+
+		// No new data, do not block.
+		if len(r.unread) == 0 {
+			return n, err
+		}
+
+		// Some new data, prepend prefix.
+		// TODO: We could write the prefix to r.unread buffer just once
+		//       and re-use it instead of prepending every time.
+		r.unread = append(r.prefix, r.unread...)
+
+		if err != nil {
+			if err == io.EOF && len(r.unread) > 0 {
+				// The underlying Reader already returned EOF, but we still
+				// have some unread data to send, thus clear the error.
 				return n, nil
 			}
 			return n, err
